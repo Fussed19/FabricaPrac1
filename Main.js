c3647b42e6c79104cd8ec1d67ec918dd2a0b7742
Fussed19 <diegopalencia19@gmail.com>
2025-10-09 12:38:00 +0000

    asdasd

--- Main.js
+++
@@ -1,8 +0,0 @@
-import { Fabrica } from "./js/Fabrica.js";
-
-const fabrica = new Fabrica();
-
-window.fabricar = function(n){
-    fabrica.fabricar(n);
-    document.getElementById("output").textContent = fabrica.exportarDatos(n);
-};
