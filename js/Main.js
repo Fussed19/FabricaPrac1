bcf1ba44b19010bef1fcf7c682e55a8ba0482ca5
Fussed19 <diegopalencia19@gmail.com>
2025-10-10 17:39:02 +0000

    asdasd

--- js/Main.js
+++ js/Main.js
@@ -1,13 +1,15 @@
 import { Fabrica } from "./Fabrica.js";
 
-let fabrica = new Fabrica();
+
 
 document.getElementById('fabricar100').addEventListener('click', () => { 
+    let fabrica = new Fabrica();
     const salida = fabrica.fabricar(100);
     document.getElementById('salida').innerText = salida; 
 });
 
 document.getElementById('fabricar1000').addEventListener('click', () => { 
+    let fabrica = new Fabrica();
     const salida = fabrica.fabricar(1000);
     document.getElementById('salida').innerText = salida; 
 });
